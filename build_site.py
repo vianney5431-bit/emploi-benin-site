c3e032bba9e3833808ebf4a09d3c726a800124c1
vianney5431-bit <vianney5431@gmail.com>
2026-07-25 21:38:54 +0100

    Update build_site.py

--- build_site.py
+++ build_site.py
@@ -19,6 +19,7 @@ TEMPLATE = """<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="google-site-verification" content="6Ielhh4Jl6872Ui1OPTWZs8RHVRGmWTxQrzYjSZQD-A" />
 <title>Offres d'emploi &amp; bourses -- Bénin &amp; International</title>
 <style>
   :root {{
